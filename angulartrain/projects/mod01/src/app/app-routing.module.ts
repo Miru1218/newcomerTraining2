3f0f78abd1ba12da141e9df6cf8c972afad9e28f
Miru <hungche0701@gmail.com>
2024-03-19 05:10:48 +0800

    feat:在 Routes 新增 perfume、reactive-forms-crud 兩個分頁

--- angulartrain/projects/mod01/src/app/app-routing.module.ts
+++ angulartrain/projects/mod01/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PerfumeComponent } from './perfume/perfume.component';
+import { ReactiveFormsCrudComponent } from './reactive-forms-crud/reactive-forms-crud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'perfume', component: PerfumeComponent },
+  { path: 'reactive-forms-crud', component: ReactiveFormsCrudComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angulartrain/projects/mod01/src/app/app.component.ts
+++ angulartrain/projects/mod01/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'mod01';
+  title = '(」・ω・)」うー！(／・ω・)／にゃー！';
 }
